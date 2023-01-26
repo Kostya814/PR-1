0f37192b741a732280c80fa39056441816ea90d8
John Doe <gnil47311@gmail.com>
2023-01-26 09:18:22 +0700

    Добавьте файлы проекта.

---
+++ PR№1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PR_1 
+{
+    class EffectiveProductionSystem
+    {
+        private List<User> users = new List<User>();
+        private List<Request> requests= new List<Request>();
+        private List<Instrument> instruments= new List<Instrument>();
+        private User currentUser;
+
+        static void Main(string[] args)
+        { 
+        
+        }
+        public void addUser(string name, string login, string password,string repeation,int type)
+        {
+            users.Add(new User(name, login, password));
+        }
+        public User findUser(string login, string password) 
+        { 
+
+            return (User)users.Where(g=> g.enter(login,password)); 
+        } 
+    } 
+}
